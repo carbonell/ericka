9c0c6b31dc7cf068de056e75af0443379ae32a0f
nicoavn <nicoavn@gmail.com>
2012-06-05 11:14:06 -0400

    Alvin 11:13 a.m. 05/06/2012

--- DBSQLDUMP.sql
+++ DBSQLDUMP.sql
@@ -113,6 +113,7 @@ CREATE TABLE `product` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(255) DEFAULT NULL,
   `price` decimal(12,2) DEFAULT NULL,
+  `quantity` int(11) DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
